4c7371d162d4fd03014b65f56a95e62b56a3d659
Harshith <60171634+Harshithhk@users.noreply.github.com>
2023-03-17 16:50:23 +0530

    init 2

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 
-const connectDB = async()=>{
-    console.log(process.env.MONGO_URI)
-    try{
-        const conn = await mongoose.connect("mongodb+srv://root:root@cluster0.nwwbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
-            useUnifiedTopology : true,
-            useNewUrlParser:true,
-            useCreateIndex:true
-            })
-            console.log(`MongoDB Connected: ${conn.connection.host}`)
-            }
-        catch(err){
-            console.log(`Error: ${err.message}`)
-            if(process.env.MODE !== "DEVELOPMENT")
-                process.exit(1)
-          }
-    }
+const connectDB = async () => {
+  console.log(process.env.MONGO_URI)
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://cluster0.sdqvr.mongodb.net/kitcoek",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      }
+    )
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.log(`Error: ${err.message}`)
+    if (process.env.MODE !== "DEVELOPMENT") process.exit(1)
+  }
+}
 
-    export default connectDB
-
-
-    
+export default connectDB
