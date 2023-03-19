035212e7634df8f87aa976891d99b5ea076b515b
Harshith <60171634+Harshithhk@users.noreply.github.com>
2023-03-19 19:36:57 +0530

    s3 disabled acls

--- server/routes/eventsRoutes.js
+++ server/routes/eventsRoutes.js
@@ -41,7 +41,6 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: "kitcoek",
-    acl: "public-read",
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.originalname })
     },
