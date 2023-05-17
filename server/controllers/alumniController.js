21c3bd13e0b50a2c690237bc7b9f5bddb6bd8c49
swapnil <swapnilhgf@gmail.com>
2023-05-17 13:22:36 +0530

    update alumni

--- server/controllers/alumniController.js
+++ server/controllers/alumniController.js
@@ -3,11 +3,26 @@ import Alumni from "../models/alumni.js"
 
 // @access Public
 const addAlumni = async (req, res) => {
+  let oldObject = await Alumni.find(req.body.email)
+  if(oldObject){
     try {
+      
+      const imageLocation = req.file ? req.file.location : null
+      req.body.photoUrl = imageLocation
+      const alumniData = await Alumni.updateOne({ _id:oldObject.id }, { $set: req.body })
+      
+      res.status(200).json({ msg: "alumni created", alumniData })
+    } catch (e) {
+      res.status(400).json(e)
+    }
+  }
+    try {
+      
       console.log(req.body)
       const imageLocation = req.file ? req.file.location : null
+      req.body.photoUrl = imageLocation
       const alumniData = await Alumni.create(req.body)
-      alumniData.photoUrl = imageLocation
+      
       res.status(200).json({ msg: "alumni created", alumniData })
     } catch (e) {
       res.status(400).json(e)
